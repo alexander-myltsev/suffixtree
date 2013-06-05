535935d6e86026007ef0bc2df0290ae3aa96edaf
Alexander Myltsev <alexander.myltsev@phystech.edu>
2013-06-05 08:07:12 +0400

    Cleanup Main class

--- src/main/java/lcs/Main.java
+++ src/main/java/lcs/Main.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.text.suffixtree.GeneralizedSuffixTree;
 import java.util.*;
 
 public class Main {
-    public static void main1(String... args) {
+    public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         int stringsCount = scanner.nextInt();
         GeneralizedSuffixTree generalizedSuffixTree = new GeneralizedSuffixTree();
@@ -18,31 +18,5 @@ public class Main {
         for (String longestCommonSubstring : generalizedSuffixTree.lcs()) {
             System.out.println(longestCommonSubstring);
         }
-    }
-
-    public static void main(String... args) {
-        List<String> inputStrings = new ArrayList<String>();
-        inputStrings.add("abxxxacaba");
-        inputStrings.add("mycabarcxxxhive");
-        inputStrings.add("acabistruxxxe");
-
-        System.out.println("-----> naive");
-        Set<String> lcsNaive = new HashSet<String>(NaiveImplementation.lcs(inputStrings));
-        for (String longestCommonSubstring : lcsNaive) {
-            System.out.println(longestCommonSubstring);
-        }
-
-        System.out.println("-----> suffix tree");
-        GeneralizedSuffixTree generalizedSuffixTree = new GeneralizedSuffixTree();
-        for (String inputString : inputStrings) {
-            generalizedSuffixTree.addSequence(inputString);
-        }
-
-        Set<String> lcsSuffixTree = new HashSet<String>(generalizedSuffixTree.lcs());
-        for (String longestCommonSubstring : lcsSuffixTree) {
-            System.out.println(longestCommonSubstring);
-        }
-
-        System.out.println("-----> sets equal: " + lcsNaive.equals(lcsSuffixTree));
     }
 }
